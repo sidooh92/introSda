3e6ed38a944350891917c60fce1fec13eca4ba14
robert <robert.piotrowski92@gmail.com>
2018-09-01 11:29:23 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        //formularz znizka
+        //https://goo.gl/forms/egDkcxePXLvtJV8O2
+    }
+}

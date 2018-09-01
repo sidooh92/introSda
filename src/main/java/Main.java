a4643f487f5db81bb05cd4cabdb40c6979992038
robert <robert.piotrowski92@gmail.com>
2018-09-01 12:20:00 +0200

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
 
+        int number = 100;
+        String text = "My first text";
+        System.out.println(text + " " + number);
+
+
+
+
+
+
         //formularz znizka
         //https://goo.gl/forms/egDkcxePXLvtJV8O2
     }
